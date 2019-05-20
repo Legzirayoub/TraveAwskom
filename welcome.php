0d14acf65b82287cc8f813554a46222d289700bf
LEGZIR AYOUB <50839444+Legzirayoub@users.noreply.github.com>
2019-05-20 23:02:42 +0200

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,11 +1,11 @@
 <?php 
 session_start();
-	include( "Aw-func/connection.php" );
-        include ( "Aw-func/Aw.php" );
+	include( "Awskomfunction/connection.php" );
+        include ( "Awskomfunction/Awskom.php" );
 	if ( isset( $_SESSION['user_email'] ) ) {
 	header( "location: home.php" );
 	}else {
               include ('forum/header.php'); 
-              include ('header/header_register.php');  
-	      include( "header/registration-form.php" );
+              include ('head/header/header_register.php');  
+	      include( "head/header/registration-form.php" );
 	      }?>
