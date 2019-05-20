db2d463a47884bc3d511db64a8cff040d45d6a1c
LEGZIR AYOUB <50839444+Legzirayoub@users.noreply.github.com>
2019-05-21 00:11:14 +0200

    Update home.php

--- home.php
+++ home.php
@@ -1,13 +1,13 @@
 <?php session_start(); ?>
 <html lang="en-US">
 <?php
-include ( "Aw-func/connection.php" );
-include ( "Aw-func/Aw.php" );
+include ( "Awskomfunction/connection.php" );
+include ( "Awskomfunction/Awskom.php" );
 if (!isset( $_SESSION['user_email'] ) ) {
 include( "forum/header.php" );
-include("Aw-func/home-bevore-login.php");?>
+include("Awskomfunction/homebevorelogin.php");?>
 <?php  
 }else {
-include ( "header/header.php" );
-include ("Aw-func/home.php"); ?>
+include ( "head/header/header.php" );
+include ("Awskomfunction/home.php"); ?>
 <?php include ("footer.php"); }  ?>
