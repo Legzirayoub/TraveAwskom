4c707498a6f4f32b7a9b0fc2424e27912354e99c
LEGZIR AYOUB <50839444+Legzirayoub@users.noreply.github.com>
2019-05-21 00:24:11 +0200

    Update displayall.php

--- Awskomfunction/displayall.php
+++ Awskomfunction/displayall.php
@@ -9,18 +9,18 @@ border-radius: 10px;
 </style>
   <meta charset="utf-8">
   <meta name="viewport" content="width=500, initial-scale=1">
-  <title>AWSKOM FLIGHT | HOTEL SEARCH AND TRAVEL FORUM</title>
+  <title>AWSKOM</title>
   <meta name="keywords" content="Flight search, hotel search, best flight and hotel, cheap air tickets, travel, travel forum">
   <meta name="description" content="Want to Compare Flight and Hotel deals or Looking for someone to travel with ? We really appreciate all of you taking time to get in touch in our travel forum">
   <meta name="apple-mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-status-bar-style" content="black">
   <meta name="format-detection" content="telephone=yes">
-  <link href="user/user_images/awskom.png" rel="shortcut icon">
+  <link href="userimage/awskom.png" rel="shortcut icon">
 </head>
 <div class="container-fluid">
 <div class="row">
 <div class="col-xs-6 col-sm-5 col-lg-7">
-<?php travel_with_all();?>
+<?php travelwithall();?>
 <div class='thumbnail'>
 </div>
 </div>
