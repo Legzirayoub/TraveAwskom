7ac51067c10c433cbf96769023a9b0b0b9288134
LEGZIR AYOUB <50839444+Legzirayoub@users.noreply.github.com>
2019-05-21 18:30:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include( "Awskomfunction/connecttodatabase.php" );
 	if ( isset( $_SESSION['user_email'] ) ) {
 	header( "location: home.php" );
 	}else {
-                include ('forum/header.php');  
+                include ('style/header.php');  
 		include( "head/header/header-login.php" );
 		include( "home-html.php/home.php" );
 		include( "login.php" );
