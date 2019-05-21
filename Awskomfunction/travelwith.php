091db63f385bfc97bde5a0fb94030c5af889374c
LEGZIR AYOUB <50839444+Legzirayoub@users.noreply.github.com>
2019-05-21 14:01:59 +0200

    Update travelwith.php

--- Awskomfunction/travelwith.php
+++ Awskomfunction/travelwith.php
@@ -1,23 +1,22 @@
-<?php   $user 	    = $_SESSION['user_email'];
-		$get_user   = "SELECT * from users where user_email='$user'";
-		$ferteh_user = mysqli_query( $connection, $get_user );
-		$row 	= mysqli_fetch_array( $ferteh_user );
-		$user_id = $row['user_id'];
+<?php           $user_email 	= $_SESSION['user_email'];
+		$get_user       = "SELECT * from users where user_email='$user_email'";
+		$ferteh_user    = mysqli_query( $connection, $get_user );
+		$row 	        = mysqli_fetch_array( $ferteh_user );
+		$user_id        = $row['user_id'];
 		$user_name 	= $row['user_name'];
 		$user_pass	= $row['user_pass'];
 		$user_email	= $row['user_email'];
 		$user_gender	= $row['user_gender'];
 		$user_image 	= $row['user_image'];
 		$register_date  = $row['register_date'];
-                $fromCoun = $row['fromCoun'];
-                $toCoun = $row['toCoun'];
-                $user_posts = "SELECT  * from posts where user_id='$user_id'";
-		$ferteh_posts = mysqli_query( $connection, $user_posts );
-		$posts = mysqli_num_rows( $ferteh_posts );
-		$sel_msg = "SELECT * from messages where receiver='$user_id' AND status='unread' order by 1 DESC";
-		$ferteh_msg = mysqli_query( $connection, $sel_msg );
-		$count_msg = mysqli_num_rows( $ferteh_msg );?>
-		
+                $fromCoun       = $row['fromCoun'];
+                $toCoun         = $row['toCoun'];
+                $user_posts     = "SELECT  * from posts where user_id='$user_id'";
+		$ferteh_posts   = mysqli_query( $connection, $user_posts );
+		$posts          = mysqli_num_rows( $ferteh_posts );
+		$sel_msg        = "SELECT * from messages where receiver='$user_id' AND status='unread' order by 1 DESC";
+		$ferteh_msg     = mysqli_query( $connection, $sel_msg );
+		$count_msg      = mysqli_num_rows( $ferteh_msg );?>
                 <div class="col-sm-9">
 			<h2>Edit Your Profile :</h2><br>
 			<form method="post" class="user-registration-form form-horizontal" enctype="multipart/form-data">
@@ -60,7 +59,7 @@
 					$ferteh = mysqli_query( $connection, $update );
 					if ( $ferteh ) {
 					echo "<script>alert('Profile Updated succesfully ')</script>";
-					echo "<script>window.open( 'home.php', '_self' )</script>";
+					echo "<script>window.open( 'home.php' )</script>";
 					}else { echo 'File is not valid. Please try again'; }}?>
 		        </div>
 <?php include( "footer.php" ); ?>
