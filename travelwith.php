96d0fe463e56966e2eb2971339303775bdbedf88
LEGZIR AYOUB <50839444+Legzirayoub@users.noreply.github.com>
2019-05-21 18:39:40 +0200

    Update travelwith.php

--- travelwith.php
+++ travelwith.php
@@ -5,7 +5,7 @@
 	if ( !isset( $_SESSION['user_email'] ) ) {
 	header( "location: index.php" );
 	}else {
-        include( "forum/header.php" );
+        include( "style/header.php" );
 	include ( "head/header/header.php" );?>
 	<?php 
 	include ("Awskomfunction/travel-with.php"); }?>
