96ba5374e03919facf692ccc6d4601ba1da5dd76
LEGZIR AYOUB <50839444+Legzirayoub@users.noreply.github.com>
2019-05-21 18:40:43 +0200

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -5,7 +5,7 @@ session_start();
 	if ( isset( $_SESSION['user_email'] ) ) {
 	header( "location: home.php" );
 	}else {
-              include ('forum/header.php'); 
+              include ('style/header.php'); 
               include ('head/header/header_register.php');  
 	      include( "head/header/registration-form.php" );
 	      }?>
