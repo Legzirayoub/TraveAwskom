dba0b91d6bc9d98d588345b3bb58b2d2d5b83884
LEGZIR AYOUB <50839444+Legzirayoub@users.noreply.github.com>
2019-05-21 18:27:55 +0200

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,1 +1,10 @@
-
+<?php
+$db['db_host'] = " ";
+$db['db_user'] = " ";
+$db['db_pass'] = " ";
+$db['db_name'] = " ";
+foreach ($db as $key => $value) {
+	define ( strtoupper( $key ), $value );
+}
+$connection = mysqli_connect( DB_HOST, DB_USER, DB_PASS, DB_NAME ) or die( "Connection was not established" );
+?>
