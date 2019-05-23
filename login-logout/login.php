84935e61bff863697228f44fa51470a18dc9eea3
LEGZIR AYOUB <50839444+Legzirayoub@users.noreply.github.com>
2019-05-23 03:56:24 +0200

    Update login.php

--- login-logout/login.php
+++ login-logout/login.php
@@ -1,1 +1,18 @@
-
+<?php 
+	if ( isset( $_POST['login']) ) {
+		$email = mysqli_real_escape_string( $connection, $_POST['email'] );
+		$pass  = mysqli_real_escape_string( $connection, $_POST['pass'] );
+		$ara_user = "SELECT * FROM users WHERE user_email='$email' AND user_pass='$pass'";
+		$ferteh_user = mysqli_query( $connection, $ara_user );
+		$security = mysqli_num_rows( $ferteh_user );
+		$Data = mysqli_fetch_array( $ferteh_user );
+		$status = $Data['status'];
+	if ( $security == 1 ) {
+	if ( $status == '1') {
+				echo "<script>alert('Your email is not verified. Please check your email Inbox.')</script>";
+	}else{	
+				$_SESSION['user_email'] = $email;
+				echo "<script>window.open('home.php')</script>";}
+	}else {
+			echo "<script>alert('Wrong Email or password')</script>";
+		}}?>
