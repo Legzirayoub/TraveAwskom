fc6fab5cd8413141b2c27bb074879ba24cf36e93
LEGZIR AYOUB <50839444+Legzirayoub@users.noreply.github.com>
2019-06-03 11:05:39 +0200

    Update registration_form.php

--- head/header/registration_form.php
+++ head/header/registration_form.php
@@ -13,7 +13,6 @@ body{
 <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
 <form action="register.php" value="register">
 <div class="container">
         <div class="row centered-form">
