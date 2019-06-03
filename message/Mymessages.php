657f84df37506f28ccca6b5231a864088d61fff4
LEGZIR AYOUB <50839444+Legzirayoub@users.noreply.github.com>
2019-06-03 19:58:24 +0200

    Create Mymessages.php

---
+++ message/Mymessages.php
@@ -0,0 +1,14 @@
+<?php 
+	session_start(); 
+        include __DIR__ . "/../Awskomfunction/connecttodatabase.php";
+        include __DIR__ . "/../Awskomfunction/Awskom.php";
+	if ( !isset( $_SESSION['user_email'] ) ) {
+	header( "location: /../index.php" );
+	}else {
+                include __DIR__ . "/../head/header/headermsg.php";
+                include __DIR__ . "/../forum/header.php";
+                include("storedmsg/M-msg.php");
+?>		                           		                                                        
+<?php 
+include ('footer.php');
+}?>
