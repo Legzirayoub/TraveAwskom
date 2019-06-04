b369a2f6d652620d226bbb6713fd01a9e0d2e1ba
LEGZIR AYOUB <50839444+Legzirayoub@users.noreply.github.com>
2019-06-04 14:39:34 +0200

    Update S.php

--- Awskomfunction/S.php
+++ Awskomfunction/S.php
@@ -9,14 +9,14 @@ border-radius: 10px;
 <div class="col-sm-9">
                 <?php
 		if ( isset( $_GET['user_id'] ) ) {
-		$user_id = $_GET['user_id'];
-		$select = "SELECT * from users where user_id='$user_id'";
-		$ferteh = mysqli_query( $connection, $select );
-		$row = mysqli_fetch_array( $ferteh);
-		$id = $row['user_id'];
-		$image = $row['user_image'];
-		$name = $row['user_name'];
-		$gender = $row['user_gender'];
+		$user_id   = $_GET['user_id'];
+		$select    = "SELECT * from users where user_id='$user_id'";
+		$ferteh    = mysqli_query( $connection, $select );
+		$row       = mysqli_fetch_array( $ferteh);
+		$user_id        = $row['user_id'];
+		$user_image     = $row['user_image'];
+		$user_name      = $row['user_name'];
+		$user_gender    = $row['user_gender'];
 		$register_date = $row['register_date']; ?>
 		<div class='panel panel-info' >
 		<div class='panel-heading'><strong>User Profile :</strong></div>
@@ -24,12 +24,12 @@ border-radius: 10px;
 		<div class='row'>
 		<div class='col-sm-8'>
 		<ul class='user-details'>
-		<li><span>Name: </span><?php echo $name ?></li>
-		<li><span>Gender: </span><?php echo $gender ?></li>
+		<li><span>Name: </span><?php echo $user_name ?></li>
+		<li><span>Gender: </span><?php echo $user_gender ?></li>
 		<li><span>Member Since: </span><?php echo $register_date ?></li>
 		</ul>
-		<a href='message/messages.php?user2_id=<?php echo $user_id ?>' class='btn btn-info'>Message <?php echo $name ?></a>
-                <a href='message/messages.php?user2_id=<?php echo $user_id ?>' class='btn btn-info'>My activity -work on it-</a>
+		<a href='message/messages.php?user2_id=<?php echo $user_id ?>' class='btn btn-info'>Message <?php echo $user_name ?></a>
+                <a href='activity/myact.php?user_id=<?php echo $user_id ?>' class='btn btn-info'>My activity -work on it-</a>
                 <a href='editpro.php?user_id=<?php echo $user_id ?>' class='btn btn-info'>Edit profile </a>
 		</div>
 		<div class='col-sm-4'>
@@ -43,20 +43,19 @@ border-radius: 10px;
 <br>
 <?php
 if ( isset( $_GET['user_id'] ) ) {
-	$user_id = $_GET['user_id'];}
-	$get_posts  = "SELECT * from posts where user_id='$user_id' ORDER by 1 DESC";
-	$ferteh_posts  = mysqli_query( $connection, $get_posts );
-	while ( $row_posts = mysqli_fetch_array( $ferteh_posts ) ) {
-		$post_id = $row_posts['post_id'];
-		$user_id = $row_posts['user_id'];
-		$post_title = $row_posts['post_title'];
-		$content = $row_posts['post_content'];
-		$post_date = $row_posts['post_date'];
-		$user = "SELECT * from users where user_id='$user_id'";
+	$user_id             = $_GET['user_id'];}
+	$get_posts           = "SELECT * from posts where user_id='$user_id' ORDER by 1 DESC";
+	$ferteh_posts        = mysqli_query( $connection, $get_posts );
+	while ( $row_posts   = mysqli_fetch_array( $ferteh_posts ) ) {
+		$post_id     = $row_posts['post_id'];
+		$user_id     = $row_posts['user_id'];
+		$content     = $row_posts['post_content'];
+		$post_date   = $row_posts['post_date'];
+		$user        = "SELECT * from users where user_id='$user_id'";
 		$ferteh_user = mysqli_query( $connection, $user );
-		$row_user = mysqli_fetch_array( $ferteh_user );
-		$user_name = $row_user['user_name'];
-		$user_image = $row_user['user_image']; ?>
+		$row_user    = mysqli_fetch_array( $ferteh_user );
+		$user_name   = $row_user['user_name'];
+		$user_image  = $row_user['user_image']; ?>
 		<div class='panel panel-primary'>
 		<div class='panel-body'>
 		<div class='col-sm-13'>
@@ -65,13 +64,14 @@ if ( isset( $_GET['user_id'] ) ) {
 		<li><a href='user_profile.php?user_id=<?php echo $user_id ?>'><?php echo $user_name ?></a></li>
 		<li><?php echo $post_date ?></li>
 		</ol>
-		<h3><?php echo $post_title ?></h3>
+		<hr>
 		<p><?php echo $content ?></p>
-                <div class='btn-group'>
+                <hr>
+		<div class='btn-group'>
                 <?php
-                $user_comments = "SELECT comment_id from comments where post_id='$post_id'";
+                $user_comments    = "SELECT comment_id from comments where post_id='$post_id'";
 		$ferteh_comments  = mysqli_query( $connection, $user_comments );
-		$comments      = mysqli_num_rows( $ferteh_comments ); ?>
+		$comments         = mysqli_num_rows( $ferteh_comments ); ?>
                 <a href='One.php?post_id=<?php echo $post_id ?>' class='navbar-brand'>View <?php echo $comments ?> comments </a>
                 <hr>
                 </div>
