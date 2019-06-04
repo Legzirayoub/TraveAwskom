8e0c18470d6d52e36e59807f8fd8af4f6efa5afd
LEGZIR AYOUB <50839444+Legzirayoub@users.noreply.github.com>
2019-06-04 14:54:22 +0200

    Update header_account.php

--- head/header/header_account.php
+++ head/header/header_account.php
@@ -4,23 +4,23 @@
 	<title>AWSKOM</title>
                 <link href="userimage/awskom.png" alt="logo" rel="shortcut icon">
                 <?php 
-		$user 	       = $_SESSION['user_email'];
-		$get_user      = "SELECT * from users where user_email='$user'";
-		$ferteh_user   = mysqli_query( $connection, $get_user );
-		$row 	       = mysqli_fetch_array( $ferteh_user );
-		$user_id       = $row['user_id'];
-		$user_name     = $row['user_name'];
-		$user_pass     = $row['user_pass'];
-		$user_email    = $row['user_email'];
-		$user_gender   = $row['user_gender'];
+		$user 	        = $_SESSION['user_email'];
+		$get_user       = "SELECT * from users where user_email='$user'";
+		$ferteh_user    = mysqli_query( $connection, $get_user );
+		$row 	        = mysqli_fetch_array( $ferteh_user );
+		$user_id        = $row['user_id'];
+		$user_name      = $row['user_name'];
+		$user_pass      = $row['user_pass'];
+		$user_email     = $row['user_email'];
+		$user_gender    = $row['user_gender'];
 		$user_image     = $row['user_image'];
 		$register_date  = $row['register_date'];
-		$user_posts = "SELECT  * from posts where user_id='$user_id'";
-		$ferteh_posts = mysqli_query( $connection, $user_posts );
-		$posts = mysqli_num_rows( $ferteh_posts );
+		$user_posts     = "SELECT  * from posts where user_id='$user_id'";
+		$ferteh_posts   = mysqli_query( $connection, $user_posts );
+		$posts          = mysqli_num_rows( $ferteh_posts );
 		?>
-                    </head>
-	                <body>
+                                        </head>
+	                                <body>
 					<div class="navbar-header">
 					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
 					<span class="sr-only"></span>
@@ -29,12 +29,12 @@
 					<span class="icon-bar"></span>
 					</button>
                     <a  href="index.php"><img src ="img/logo.png" width="70px" height="70px"></a></div>				
-					<div class="collapse navbar-collapse navbar-ex1-collapse">
+		    <div class="collapse navbar-collapse navbar-ex1-collapse">
                     <div class="navbar-form navbar-left">                                                            
 <a href="logout.php"><button class="btn btn-info"><img src ="img/users.png" width="25px" alt="users" height="25px">Logout</button></a>
 <a href="home.php"><button class="btn btn-info"><img src ="img/1f3d5.png" alt="forum" width="25px" height="25px">Forum </button></a>				
-                    </div><form action="" method="post" class="navbar-form navbar-right" role="search">
-			        <div class="form-group">                                                            
+</div><form action="" method="post" class="navbar-form navbar-right" role="search">
+<div class="form-group">                                                            
 <form method="get" action="results.php" id="form1" class="navbar-form navbar-right" role="search">
 			<div class="form-group">
 			<input type="text" name="user_query" class="form-control" placeholder="Search a topic">
@@ -45,4 +45,5 @@
 			<div class="form-group"> 	
 <a href="user_profile.php?user_id=<?php echo $user_id; ?>" >  <img src="userimage/<?php echo $user_image; ?>" class="img-circle" width="50px" height="50px"alt="users"><?php echo $user_name; ?></a>
 </div>
-</form></div> 
+</form>
+</div> 
