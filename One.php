b4ceaad1f3144300912db3d87c8da95a5e21b1d1
LEGZIR AYOUB <50839444+Legzirayoub@users.noreply.github.com>
2019-06-04 14:33:43 +0200

    Update One.php

--- One.php
+++ One.php
@@ -10,20 +10,19 @@
 		<div class="col-xs-7 col-sm-6 col-lg-8">
 		<div id="posts">
 		<?php if ( isset( $_GET['post_id'] ) ) {
-		$get_id = $_GET['post_id'];
-		$get_posts  = "SELECT * from posts where post_id='$get_id'";
+		$get_id        = $_GET['post_id'];
+		$get_posts     = "SELECT * from posts where post_id='$get_id'";
 		$ferteh_posts  = mysqli_query( $connection, $get_posts );
-		$row_posts = mysqli_fetch_array( $ferteh_posts );
-		$post_id = $row_posts['post_id'];
-		$user_id = $row_posts['user_id'];
-		$post_title = $row_posts['post_title'];
-		$content = $row_posts['post_content'];
-		$post_date = $row_posts['post_date'];
-		$user = "SELECT * from users where user_id='$user_id' AND posts='1'";
-		$ferteh_user = mysqli_query( $connection, $user );
-		$row_user = mysqli_fetch_array( $ferteh_user );
-		$user_name = $row_user['user_name'];
-		$user_image = $row_user['user_image']; ?>
+		$row_posts     = mysqli_fetch_array( $ferteh_posts );
+		$post_id       = $row_posts['post_id'];
+		$user_id       = $row_posts['user_id'];
+		$content       = $row_posts['post_content'];
+		$post_date     = $row_posts['post_date'];
+		$user          = "SELECT * from users where user_id='$user_id' AND posts='1'";
+		$ferteh_user   = mysqli_query( $connection, $user );
+		$row_user      = mysqli_fetch_array( $ferteh_user );
+		$user_name     = $row_user['user_name'];
+		$user_image    = $row_user['user_image']; ?>
                 <div class='panel panel-default'>
 		<div class='panel-body'>
 		<div class='col-sm-13'>
@@ -32,33 +31,35 @@
 		<li><a href='user_profile.php?user_id=<?php echo $user_id ?>'><?php echo $user_name ?></a></li>
 		<li><?php echo $post_date ?></li>
 		</ol>
+		<hr>
 		<p><?php echo $content ?></p>
+		<hr>
 		</div>
 		</div>
 		</div>
 		<?php
 		if ( isset( $_POST['reply'] ) ) {
-			$comment = $_POST['comment'];
-			$user_email = $_SESSION['user_email'];
-			$current_user = "SELECT * from users where user_email='$user_email'";
+			$comment             = $_POST['comment'];
+			$user_email          = $_SESSION['user_email'];
+			$current_user        = "SELECT * from users where user_email='$user_email'";
 			$ferteh_current_user = mysqli_query( $connection, $current_user );
-			$current_user_row = mysqli_fetch_array( $ferteh_current_user );
-			$current_user_id = $current_user_row['user_id'];
-			$insert = "INSERT into comments (post_id, user_id,comment,date) values('$post_id','$current_user_id','$comment',NOW())";
-			$ferteh = mysqli_query( $connection, $insert );
+			$current_user_row    = mysqli_fetch_array( $ferteh_current_user );
+			$current_user_id     = $current_user_row['user_id'];
+			$insert              = "INSERT into comments (post_id, user_id,comment,date) values('$post_id','$current_user_id','$comment',NOW())";
+			$ferteh              = mysqli_query( $connection, $insert );
 			}  
-	        $get_id = $_GET['post_id'];
-	        $get_comments = "SELECT * from comments where post_id='$post_id'";
-	        $ferteh_query = mysqli_query( $connection, $get_comments );
-	        while   ( $row = mysqli_fetch_array( $ferteh_query ) ) {
+	        $get_id          = $_GET['post_id'];
+	        $get_comments    = "SELECT * from comments where post_id='$post_id'";
+	        $ferteh_query    = mysqli_query( $connection, $get_comments );
+	        while   ( $row   = mysqli_fetch_array( $ferteh_query ) ) {
 		$comment_user_id = $row['user_id'];
-		$comment = $row['comment'];
-		$comment_date = $row['date'];
-		$user = "SELECT * from users where user_id='$comment_user_id'";
-		$ferteh_user = mysqli_query( $connection, $user );
-		$row_user = mysqli_fetch_array( $ferteh_user );
-		$user_name = $row_user['user_name'];
-		$user_image = $row_user['user_image'];
+		$comment         = $row['comment'];
+		$comment_date    = $row['date'];
+		$user            = "SELECT * from users where user_id='$comment_user_id'";
+		$ferteh_user     = mysqli_query( $connection, $user );
+		$row_user        = mysqli_fetch_array( $ferteh_user );
+		$user_name       = $row_user['user_name'];
+		$user_image      = $row_user['user_image'];
                 ?>
 		<div class='row single-comment'>
 		<div class='col-sm-10'>
